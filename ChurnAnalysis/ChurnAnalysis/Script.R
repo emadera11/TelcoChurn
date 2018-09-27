c15ae416d85c9ffa3e6a017b11845a6f7408679d
emadera11 <43622403+emadera11@users.noreply.github.com>
2018-09-27 01:43:21 -0400

    added few markdown tables

--- ChurnAnalysis/ChurnAnalysis/Script.R
+++ ChurnAnalysis/ChurnAnalysis/Script.R
@@ -1,6 +1,14 @@
 library(dplyr)
+library(psych)
+library(tidyr)
 library(ggplot2)
 library(rpart)
 library(rpart.plot)
 library(caTools)
 library(randomForest)
+library(knitr)
+
+
+df = read.csv("customerchurn.csv", na.strings = c("", "NA"))
+
+head(df)
