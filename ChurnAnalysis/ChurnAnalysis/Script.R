ed692e39249df57a6e93b270925ad587d21a0e18
emadera11 <43622403+emadera11@users.noreply.github.com>
2018-09-28 20:04:23 -0400

    changes to rmarkedown

--- ChurnAnalysis/ChurnAnalysis/Script.R
+++ ChurnAnalysis/ChurnAnalysis/Script.R
@@ -7,8 +7,25 @@ library(rpart.plot)
 library(caTools)
 library(randomForest)
 library(knitr)
+library(devtools)
+
+install_github("haozhu233/kableExtra")
 
 
 df = read.csv("customerchurn.csv", na.strings = c("", "NA"))
 
 head(df)
+
+install.packages("kableExtra")
+
+update.packages("kabelExtra")
+install.packages("randomForest")
+install.packages("ggplot2")
+install.packages("psych")
+install.packages("tidyr")
+install.packages("rpart")
+install.packages("rpart.plot")
+install.packages("dplyr")
+
+install.packages("devtools")
+install.packages("knitr")
