5f9e815cd2cea487f6d78fac478cc711c1f6396a
emadera11 <43622403+emadera11@users.noreply.github.com>
2018-10-03 16:30:38 -0400

    new change

--- ChurnAnalysis/ChurnAnalysis/Script.R
+++ ChurnAnalysis/ChurnAnalysis/Script.R
@@ -8,17 +8,11 @@ library(caTools)
 library(randomForest)
 library(knitr)
 library(rmarkdown)
-library(markdown)
 library(kableExtra)
 library(gridExtra)
 library(corrplot)
 library(caret)
 library(e1071)
-
-
-
-
-
 
 #loading data 
 
@@ -27,6 +21,11 @@ df = read.csv("D:\\Emman\\Documents\\R Projects\\Datasources\\customerchurn.csv"
 #check the first 5 rows of the data
 
 head(df)
+
+#df$MultipleLines = as.factor(ifelse(as.character(df$MultipleLines) == "No phone service", "No", as.character(df$MultipleLines))) %>% factor(,levels = c("No", "Yes"), labels = c(0,1))
+
+
+summary(df)
 
 str(df)
 
@@ -40,9 +39,8 @@ colSums(is.na(df))
 #the min value is 18.50 and the max value is 8684.80 this indicates variation in the data.  
 #We will use outlier detection and treament
 summary(df)
-psych::describe(df)
 
-
+#data visualization
 c1 = ggplot(df, aes(MonthlyCharges)) +
     geom_histogram(binwidth = 10, color = "black", fill = "blue") +
     ggtitle("Distribution of Monthly Charges") +
@@ -70,8 +68,6 @@ a2 = df %>% select(Contract, MonthlyCharges) %>%
 grid.arrange(a1, a2, ncol = 2)
 
 
-
-
 b1 = ggplot(df, aes(factor(Contract), fill = Contract)) +
     geom_bar()
 
@@ -86,97 +82,95 @@ grid.arrange(b1, b2, ncol = 2)
 ggplot(df, aes(x = TotalCharges, y = tenure, color = gender)) +
     geom_point()
 
-
+#CLeaning the data 
 #remove customer id from equation data preparation
 #df$OnlineSecurity = as.factor(ifelse( as.character(df$OnlineSecurity) == "No internet service", "No",as.character(df$OnlineSecurity)))
 
 for (i in names(df)) {
     if (is.factor(df[, i]) == TRUE) {
-        df[, i] = as.factor(ifelse(as.character(df[, i]) == "No internet service" | as.character(df[, i]) == "No phone service", " No ", as.character(df[, i])))
-      
+        df[, i] = as.factor(ifelse(as.character(df[, i]) == "No internet service" | as.character(df[, i]) == "No phone service", "No", as.character(df[, i])))
+        
+    }
+    if (is.factor(df[, i]) & nlevels(df[, i]) == 2) {
+        if (i == "gender") {
+            df[, i] = factor(df[, i], levels = c("Female", "Male"), labels = c(0, 1))
+        } else {
+            df[, i] = factor(df[, i], levels = c("No", "Yes"), labels = c(0, 1))
+        }
     }
 }
 
-df$Churn = ifelse(df$Churn == "Yes", 1, 0)
+df$TotalCharges = ifelse(is.na(df$TotalCharges), mean(df$TotalCharges, na.rm = TRUE), df$TotalCharges)
 
-df %>% group_by(Contract) %>% summarise(Total = n()) %>% ggplot(aes(Contract, Total)) + geom_bar(stat = "identity")
+df$InternetService = factor(df$InternetService,
+                            levels = c("DSL", "Fiber optic", "No"),
+                            labels = c(1, 2, 3))
 
-
-b = df %>% select(-customerID)
-
-
-indx <- sapply(b, is.factor)
-b[indx] <- lapply(b[indx], function(x) as.integer((x)))
-
-head(b)
-
-str(b)
+df$Contract = factor(df$Contract,
+                     levels = c("Month-to-month", "One year", "Two year"),
+                     labels = c(1,2,3))
 
 
+for (i in colnames(df)) {
+    if (is.numeric(df[, i]) & i != "SeniorCitizen") {
+        df[, i] = scale(df[, i])
+    }
+}
 
-c = cor(b)
-corrplot(c, method = 'circle')
+summary(df)
+
+str(df)
+
+final = df %>% select(-customerID, -PaymentMethod)
 
 #split train to test
-dt = sort(sample(nrow(b), nrow(b) * 0.8))
-train = b[dt,]
-test = b[-dt,]
+split = sample.split(final$Churn, SplitRatio = 0.80)
+train = subset(final, split == TRUE)
+test = subset(final, split == FALSE)
 
 head(train)
 head(test)
 
-str(train)
-
-
-
 #model building
 
-model1 = glm(Churn ~ SeniorCitizen + tenure + PhoneService + InternetService +
-    OnlineSecurity + OnlineBackup + DeviceProtection + TechSupport +
-    StreamingTV + StreamingMovies + Contract + PaperlessBilling +
-    MonthlyCharges + TotalCharges, data = train, family = binomial(link = "logit"))
+formula = "Churn ~ ."
 
-abc = stepAIC(model, direction = 'both')
-summary(model)
+model1 = glm(formula, data = train, family = binomial(link = "logit"))
 
-abc$anova
+abc = step(model1, direction = 'both')
+summary(model1)
 
-factor(test$Churn)
+abc$formula
 
-fitted.results = predict(model1, newdata = test, type = 'response')
+formula2 ="Churn ~ SeniorCitizen + tenure + MultipleLines + InternetService + 
+    OnlineSecurity + DeviceProtection + TechSupport + StreamingTV + 
+    StreamingMovies + Contract + PaperlessBilling + MonthlyCharges + 
+    TotalCharges"
+
+model2 = glm(formula2, data = train, family = binomial(link = "logit"))
+zyx = step(model2, direction = "both")
+summary(model2)
+
+formula3 = "Churn ~ SeniorCitizen + tenure + MultipleLines + InternetService +
+    OnlineSecurity + StreamingTV + StreamingMovies + Contract + PaperlessBilling + MonthlyCharges +
+    TotalCharges"
+
+
+model3 = glm(formula3, data = train, family = binomial(link = "logit"))
+zyx = step(model3, direction = "both")
+summary(model3)
+
+
+fitted.results = predict(model3, newdata = test, type = 'response')
 fitted.results <- ifelse(fitted.results > 0.5, 1, 0)
 misClasificError <- mean(fitted.results != test$Churn)
 print(paste('Accuracy', 1 - misClasificError))
 
 confusionMatrix(factor(fitted.results), factor(test$Churn))
 
-vif(model)
-
-model2 = glm(Churn ~ SeniorCitizen + tenure + PhoneService +
-    OnlineSecurity + OnlineBackup + DeviceProtection + TechSupport + Contract + PaperlessBilling + MonthlyCharges +
-    TotalCharges, data = train, family = binomial(link = "logit"))
-
-summary(model2)
-
-anova(model2)
-
-vif(model2)
-
-predict()
-
-fitted.results <- predict(model, newdata = test, type = 'response')
-fitted.results <- ifelse(fitted.results > 0.5, 1, 0)
-misClasificError <- mean(fitted.results != test$Churn)
-print(paste('Accuracy', 1 - misClasificError))
-
-confusionMatrix(test$Churn, fitted.results)
-
 library(rpart.plot)
 library(rpart)
-tree = rpart(Churn ~ SeniorCitizen + tenure + PhoneService + InternetService +
-    OnlineSecurity + OnlineBackup + DeviceProtection + TechSupport +
-    StreamingTV + StreamingMovies + Contract + PaperlessBilling +
-    MonthlyCharges + TotalCharges, method = "class", data = train)
+tree = rpart(formula3, method = "class", data = train)
 
 printcp(tree)
 
@@ -186,34 +180,21 @@ a = predict(tree, test, type = "class")
 
 confusionMatrix(factor(test$Churn), factor(a))
 
-head(test$Churn)
-
 summary(tree)
 
+head(a)
 
-
-model1 = glm(Churn ~ tenure + PhoneService + OnlineSecurity + OnlineBackup + TechSupport + Contract + PaperlessBilling +
-    MonthlyCharges + TotalCharges, data = train, family = binomial(link = "logit"))
-
-summary(model1)
-
-anova(model1)
-
-plot(model1)
-plot(anova(model1))
-
-
-fitted.results <- predict(model1, newdata = test, type = 'response')
-fitted.results <- ifelse(fitted.results > 0.5, 1, 0)
+fitted.results <- predict(tree, newdata = test, type = "class")
 misClasificError <- mean(fitted.results != test$Churn)
 print(paste('Accuracy', 1 - misClasificError))
 
 confusionMatrix(fitted.results, test$Churn)
 
+install.packages("ROCR")
 
 library(ROCR)
 
-p = predict(model1, newdata = test, type = "response")
+p = predict(model3, newdata = test, type = "response")
 pred = prediction(p, test$Churn)
 
 
@@ -224,15 +205,6 @@ auc <- performance(pred, measure = "auc")
 auc <- auc@y.values[[1]]
 auc
 
-
-df %>% select(Contract) %>% head
-
-cmd = df %>%
-    select(Contract, MonthlyCharges) %>%
-    data.frame()
-
-head(cmd)
-
 #anova anlyiss on contrct and monthly Charges 
 #H0 = The means are equal between contracts
 #Ha = The Means are different between the contracts.   
@@ -241,4 +213,6 @@ anova = aov(MonthlyCharges ~ Contract, df)
 summary(anova)
 
 #Pairwise
-TukeyHSD(anova)
+t = TukeyHSD(anova)
+
+plot(t)
